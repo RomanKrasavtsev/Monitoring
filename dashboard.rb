8df8924ec490023b270f4d2486b9d0b25c529fcd
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-10-17 17:01:49 +0300

    add keenio class

--- dashboard.rb
+++ dashboard.rb
@@ -1,9 +1,19 @@
 require 'sinatra'
 
+class KeenIO
+  def self.get_values_as_charts
+    "Charts"
+  end
+
+  def self.get_values_as_json
+  end
+end
+
 get '/' do
   <<-EOF
     <h1>
       Dashboard
     </h1>
+    #{KeenIO.get_values_as_charts}
   EOF
 end
