5d6c461dd4f1b973f7a1a62cabd75aaa9de7e9aa
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-10-25 15:29:27 +0300

    update ssh

--- lib/ssh.rb
+++ lib/ssh.rb
@@ -3,16 +3,16 @@
 require "net/ssh"
 
 class Ssh
-  attr_reader :host
-  
-  def initialize host, user, password
-    @host = host
+  attr_reader :ip
+
+  def initialize ip, user, password
+    @ip = ip
     @user = user
     @password = password
   end
 
   def exec command
-    Net::SSH.start(@host, @user, password: @password) do |ssh|
+    Net::SSH.start(@ip, @user, password: @password) do |ssh|
       ssh.exec!(command)
     end
   end
