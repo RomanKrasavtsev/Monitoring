22654c6333a8cb997515ab6f18ba960c235ed442
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-10-26 15:58:20 +0300

    update schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,5 +4,13 @@ set :job_template, "/bin/zsh -l -c ':job'"
 set :output, "#{Dir.pwd}/monitoring.log"
 
 every 1.minutes do
- command "#{Dir.pwd}/monitoring.rb"
+  command "#{Dir.pwd}/monitoring.rb system_cpu"
+  command "#{Dir.pwd}/monitoring.rb system_memory"
+  command "#{Dir.pwd}/monitoring.rb system_vm"
+  command "#{Dir.pwd}/monitoring.rb mysql_threads_connected"
+end
+
+every 10.minutes do
+  command "#{Dir.pwd}/monitoring.rb mysql_database_size"
+  command "#{Dir.pwd}/monitoring.rb system_df"
 end
