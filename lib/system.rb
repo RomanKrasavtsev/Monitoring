7a37fe58dd4cf502675691f96d5684cbfb69e506
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-10-26 15:50:35 +0300

    update system

--- lib/system.rb
+++ lib/system.rb
@@ -5,8 +5,9 @@ class System
     "top -b -n1 | grep \"Cpu(s)\" | awk '{print $2 + $3}'"
   end
 
-  def free_space filesystem
-    "df -h #{filesystem} | tail -n1 | cut -d' ' -f7"
+  def get_free_space filesystem
+    # Size Avail Use%
+    "df -m #{filesystem} | tail -n1 | awk '{print $1, $3, $4}'"
   end
 
   def get_memory_usage
